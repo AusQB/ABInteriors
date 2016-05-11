a667f515e72601f3e8142be10d9a8ec19956f3b2
Robert Gillman <rgillman88@gmail.com>
2016-05-11 19:39:12 +0800

    Initial commit

---
+++ www/public_html/themes/third_party/wyvern/wysiwyg.js
@@ -0,0 +1,7 @@
+CKEDITOR.addStylesSet('wyvern', [
+    { name: 'Paragraph', element: 'p' },
+    { name: 'Heading', element: 'h4' },
+    { name: 'Image on left', element: 'img', attributes: { 'class': 'float_left' }},
+    { name: 'Image on right', element: 'img', attributes: { 'class': 'float_right' }},
+    { name: 'Blockquote', element: 'p', attributes: { 'class': 'quote' }}
+]);
